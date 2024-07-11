0703212cba939ff1d44f17f40f570f66e12d7be9
Yuri Barbosa <127057738+YuriGit909@users.noreply.github.com>
2024-07-11 10:13:33 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,49 @@
-const activity = document.querySelector('input[name="activity"]')
-const date = document.querySelector('input[name="date"]')
-const time = document.querySelector('input[name="time"]')
-const button = document.querySelector('input[type="button"]')
+// SELEÇÃO DOS ELEMENTOS DO DOM
+let activity = document.querySelector('input#idActivity').value
+let date = document.querySelector('input#idDate').value
+let time = document.querySelector('input#idTime').value
+let button = document.querySelector('input#btn')
+let activityList = document.querySelector('section#activityList')
 
+// FUNÇÃO PARA FORMATAR DATA PARA O FORMATO DD/MM/AAAA
+let dateFormatted = (data) => {
+    // OBJETO DATE PASSANDO COMO DADO O PARAMÊTRO DA FUNÇÃO
+    let dateObject = new Date(data)
+
+    // FORMATAÇÃO DOS DIAS, MESES E ANOS SEPARADAMENTE
+    // O + 1 EM "getDate()" E "getMonth()" É USADO POIS A CONTAGEM DOS DIAS E MESES INICIA EM 0 (PADRÃO)
+    let day = (dateObject.getDate() + 1).toString().padStart(2, '0')
+    let month = (dateObject.getMonth() + 1).toString().padStart(2, '0')
+    let year = dateObject.getFullYear()
+
+    // RETORNO DOS DADOS
+    return `${day}/${month}/${year}`
+}
+
+// FUNÇÃO PARA EXECUÇÃO DAS AÇÕES ATRAVÉS "click" DO BOTÃO
 button.addEventListener('click', () => {
-    let activityList = document.querySelector('section#activityList')
-    let activityData = activity.value
-    let dateData = date.value
-    let timeData = time.value
+    // ATUALIZAÇÃO DOS DADOS DOS SELETORES PÓS-FUNÇÃO
+    activity = document.querySelector('input#idActivity').value
+    date = document.querySelector('input#idDate').value
+    time = document.querySelector('input#idTime').value
 
-    activityList.innerHTML = `${activity}, ${date}, ${time}`
+
+    activityList.innerHTML = `${activity}, ${dateFormatted(date)} ${time}`
 })
 
-/*
-idTime
-idDate
-idActivity
-idLocate
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
